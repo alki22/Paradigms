124e754a0479828497d39910cecdffc89305c2a3
Cristian Cardellino <crscardellino@gmail.com>
2016-05-05 15:59:40 -0300

    Laboratorio 4. Consignas y esqueleto base

---
+++ Lab4/settings.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+APP_DIR = os.path.dirname(os.path.realpath(__file__))
+DATABASE = 'sqlite:///{}'.format(os.path.join(APP_DIR, 'app.db'))
+DEBUG = True
+SECRET_KEY = 'development'
