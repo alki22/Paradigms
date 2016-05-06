d69abf42ed9f746897cb079fa080dd942cc04d51
Ignacio Tiraboschi <ignatirabo@gmail.com>
2016-05-06 11:43:35 -0300

    El Main va tomando forma

--- Lab3/src/main.ml
+++ Lab3/src/main.ml
@@ -1,4 +1,17 @@
 open Deck
 open Player
+open Pervasives
+open Printf
 
-let _ = 
+let players = []
+
+let addPlayer =
+    let name = read_line() in
+    if name = "EXIT" then (0,"EXIT")
+    else (1,name)
+
+let rec getPlayers (s : list player) =
+    let got = addPLayer() in
+    match got with
+    | (0,_) -> s
+    | (1,name) -> 
