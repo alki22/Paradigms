30a70479f58b38f6b2cae99ec855769f1e074c56
Ignacio Tiraboschi <ignatirabo@gmail.com>
2016-05-20 13:17:57 -0300

    Se pueden agregar feeds

--- Lab4/runserver.py
+++ Lab4/runserver.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import feedparser
+
 from app import app, database, login_manager
 from auth import *
 from flask import render_template, request, session
@@ -28,9 +30,22 @@ def login():
         return login_github()
     return render_template('login.html')
 
-@app.route("/new_feed")
+@app.route("/delete_feed")
+def delete_feed():
+    return "Miameee"
+
+@app.route("/new_feed", methods=['GET', 'POST'])
 @login_required
 def new_feed():
+    import ipdb; ipdb.set_trace()
+    if request.method == 'POST':
+        url = request.form['feed_url']
+        d = feedparser.parse(url)
+        feed = Feed.select().where(Feed.url == url)
+        if len(feed) == 0:
+            feed = Feed.create(url = url, title = d['feed']['title'], description = d['feed']['subtitle'], user = current_user.id)
+            return render_template('index.html')
+
     return render_template('newfeed.html')
 
 @app.route("/index")
@@ -48,6 +63,10 @@ def create(nickname, social_id):
 def read(social_id):
     user = get_object_or_404(User.select(), User.social_id==social_id)
     return "<h1>{}</h1>".format(user.nickname)
+
+@app.route("/rss")
+def rss():
+    return "Miameee"
 
 def main():
     database.create_tables([User, Feed], safe=True)
