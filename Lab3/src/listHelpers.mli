785957cdb97c4c80bae96897048a21193ad80b82
Ignacio Tiraboschi <ignatirabo@gmail.com>
2016-06-12 20:35:10 -0300

    Innecesario

--- Lab3/src/listHelpers.mli
+++
@@ -1,5 +0,0 @@
-(* Chequea si un elemento pertenece a la lista *)
-val elemInList : 'a list -> 'a -> bool
-
-(* Devuelve una copia de la lista dada sin el k-ésimo elemento *)
-val listDeleteElem : 'a list -> int -> 'a list
