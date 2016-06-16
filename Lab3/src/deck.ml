b43b879604ae10efaf3cb3dbcbfed975237a4b09
Ignacio Tiraboschi <ignatirabo@gmail.com>
2016-06-16 15:26:32 -0300

    Compila el módulo deck!

--- Lab3/src/deck.ml
+++ Lab3/src/deck.ml
@@ -25,13 +25,16 @@ let deck_full =
   List.map (fun suit -> {value = num; suit = suit}) normal_suits in
   specials @ (List.concat (List.map make_value_cards values))
 
-(* Repartir las cartas iniciales *)
-let deal_hand (hand:deck) (deck:deck) =
-  match deck with
-  | [] -> invalid_arg "Empty deck"
-  | xs -> let took, left = deck_draw xs 7 in
-      (hand @ took, left)
-    
+(* Toma una carta del mazo dado *)
+(* Debería levantar excepción *)
+let deck_draw_single (deck:deck) =
+  let l = List.length deck in
+  match l with
+  | 0 ->  failwith "Empty deck"
+  | _ ->  let r = Random.int l in
+          let card = List.nth deck r in
+          (card, List.filter (fun x -> x <> card) deck)
+
 (* Toma n cartas del mazo principal *)
 let deck_draw (deck:deck) (num:int) =
   let rec deck_draw' acc deck num = 
@@ -43,53 +46,51 @@ let deck_draw (deck:deck) (num:int) =
   in
   deck_draw' [] deck num
 
-(* Toma una carta del mazo dado *)
-(* Debería levantar excepción *)
-let deck_draw_single (deck:deck) =
-  let l = List.length deck in
-  match l with
-  | 0 ->  raise EMPTY_DECK
-  | _ ->  let r = Random.int l in
-          let card = List.nth deck r in
-          (card, List.filter (fun x -> x <> card) deck)
+(* Repartir las cartas iniciales *)
+let deal_hand (hand:deck) (deck:deck) =
+  match deck with
+  | [] -> invalid_arg "Empty deck"
+  | xs -> let took, left = deck_draw xs 7 in
+      (hand @ took, left)
 
 let deck_size (deck:deck) =
   List.length deck
 
 (* Ordena las cartas, la primera es la mayor, la segunda la menor *)
 let sort_card (card1:card) (card2:card) =
-  if (snd card1) > (snd card2) then (card1, card2, true)
-  else if (snd card1) < (snd card2) then (card2, card1, false)
+  if card1.value > card2.value then (card1, card2, true)
+  else if card1.value < card2.value then (card2, card1, false)
   else
     begin
-    if (fst card1) = "E" then (card1, card2, true)
-    else if (fst card2) = "E" then (card2, card1, false)
-    else if (fst card1) = "B" then (card1, card2, true)
-    else if (fst card2) = "B" then (card2, card1, false)
-    else if (fst card1) = "O" then (card1, card2, true)
-    else (card2, card1, false)
+      if card1.suit = Espada then (card1, card2, true)
+      else if card2.suit = Espada then (card2, card1, false)
+      else if card1.suit = Basto then (card1, card2, true)
+      else if card2.suit = Basto then (card2, card1, false)
+      else if card1.suit = Oro then (card1, card2, true)
+      else (card2, card1, false)
+    end
 
 (* Devuelve la máxima carta *)
 let max_card (card1:card) (card2:card) =
-  let card1, card2, b = sort_card card1, card2 in
+  let card1, card2, b = sort_card card1 card2 in
   card1
 
 (* Devuelve la mínima carta *)
 let min_card (card1:card) (card2:card) =
-  let card1, card2, b = sort_card card1, card2 in
+  let card1, card2, b = sort_card card1 card2 in
   card2
 
-(* Devuelve la máxima carta del mazo, y el mazo sin la carta *)
+(* Estilo de filter, pero hace más cosas *)
 let deck_get (deck:deck) f =
+  let rec deck_get' xs x =
+    match xs with
+    | [] -> x
+    | y :: ys ->  let card = f x y in
+                  deck_get' ys card
+  in
   match deck with
-  | [] -> 
-  | x :: xs ->  let deck_get' xs x =
-                  match xs with
-                  | [] -> x
-                  | y :: ys ->  let card = f x y in
-                                deck_get' ys card
-                in
-                let card = deck_get' xs x in
+  | [] -> failwith "No cards, fix"
+  | x :: xs ->  let card = deck_get' xs x in
                 (card, List.filter (fun x -> x <> card) deck)
 
 let is_normal (card:card) =
@@ -97,33 +98,45 @@ let is_normal (card:card) =
 
 (* Separa las cartas pares, de las impares, del mazo *)    
 let get_even_cards (deck:deck) =
-  let even = List.filter (fun x -> ((snd x) mod 2) = 0) deck in
-  let odd = List.filter (fun x -> ((snd x) mod 2) = 0) deck in
+  let even = List.filter (fun x -> (x.value mod 2) = 0) deck in
+  let odd = List.filter (fun x -> (x.value mod 2) = 0) deck in
   (even, odd)
 
 (* Dado un string, parseamos una carta *)
 let string_to_card c =
   let len = (String.length c - 1)  in
-  let possible_int = (String.sub c 1 len)
-  in
+  let possible_int = (String.sub c 1 len) in
   try
-  match String.get c 0 with
-    'E' -> {value = int_of_string possible_int; suit = Espada}
-  | 'C' -> {value = int_of_string possible_int; suit = Copa}
-  | 'O' -> {value = int_of_string possible_int; suit = Oro}
-  | 'B' -> {value = int_of_string possible_int; suit = Basto}
-  | 'S' -> (match String.sub c 1 2 with
-              "ID" -> {value = -1; suit = ID}
-            | "SW" -> {value = -1; suit = SWAP}
-            | "MA" -> {value = -1; suit = MAX}
-            | "MI" -> {value = -1; suit = MIN}
-            | "TO" -> {value = -1; suit = TOP}
-            | "PA" -> {value = -1; suit = PAR}
-            | _ -> invalid_arg "No such card.")
-  | _ -> invalid_arg "No such card."
+    match String.get c 0 with
+    | 'E' -> [{value = int_of_string possible_int; suit = Espada}]
+    | 'C' -> [{value = int_of_string possible_int; suit = Copa}]
+    | 'O' -> [{value = int_of_string possible_int; suit = Oro}]
+    | 'B' -> [{value = int_of_string possible_int; suit = Basto}]
+    | 'S' -> (match String.sub c 1 2 with
+              | "ID" -> [{value = -1; suit = ID}]
+              | "SW" -> [{value = -1; suit = SWAP}]
+              | "MA" -> [{value = -1; suit = MAX}]
+              | "MI" -> [{value = -1; suit = MIN}]
+              | "TO" -> [{value = -1; suit = TOP}]
+              | "PA" -> [{value = -1; suit = PAR}]
+              | _ -> invalid_arg "No such card.")
+    | _ -> invalid_arg "No such card."
   with Failure "int_of_string" -> (print_endline "No es una carta valida!"; [])
 
 (** Funciones que printean **)
+
+let rec print_card (target:card) =
+  match target.suit with
+  | Espada -> print_char 'E'; print_int target.value
+  | Oro -> print_char 'O'; print_int target.value
+  | Copa -> print_char 'C'; print_int target.value
+  | Basto -> print_char 'B'; print_int target.value
+  | ID -> print_string "SID"
+  | SWAP -> print_string "SSWAP"
+  | MAX -> print_string "SMAX"
+  | MIN -> print_string "SMIN"
+  | TOP -> print_string "STOP"
+  | PAR -> print_string "SPAR"
 
 let rec print_deck (target:deck) =
   match target with
--- Lab3/src/deck.mli
+++ Lab3/src/deck.mli
@@ -1,14 +1,22 @@
+type suit = Espada | Copa | Oro | Basto |  ID | SWAP | MAX | MIN | TOP | PAR
 type card = { suit : suit; value: int }
 type deck = card list
 
+val deck_empty : deck
 val deck_full : deck
 
-val deal_hand : deck -> deck ->  deck * deck
-
-val deck_draw : deck -> int -> deck * deck
-
+(* Funciones *)
 val deck_draw_single : deck -> card * deck
+val deck_draw : deck -> int -> deck * deck
+val deal_hand : deck -> deck ->  deck * deck
+val deck_size : deck -> int
 
+val sort_card : card -> card -> card * card * bool
 val max_card : card -> card -> card
+val min_card : card -> card -> card
+val deck_get : deck -> (card -> card -> card) -> card * deck
+val is_normal : card -> bool
+val get_even_cards : deck -> deck * deck
+val string_to_card : string -> deck
 
-val get_even_cards : deck -> deck
+val print_deck : deck -> unit
